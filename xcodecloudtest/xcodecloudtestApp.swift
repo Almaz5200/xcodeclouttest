e4d1bb822b0ffe28f2f65359ed5dadb0c8e04fcf
Artem Trubacheev <almaz5200@gmail.com>
2022-06-16 20:31:54 +0300

    hello world

---
+++ xcodecloudtest/xcodecloudtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodecloudtestApp.swift
+//  xcodecloudtest
+//
+//  Created by Artem Trubacheev on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct xcodecloudtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
